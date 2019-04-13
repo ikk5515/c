d8e30c2073d2045f11bfcaa690884e1f6786e8f6
ikk5515 <ikk5515@naver.com>
2019-04-14 01:53:54 +0900

    C

--- BitLeftShift.c
+++ BitLeftShift.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-     int num = 15;
+    int num = 15;
 
-    int result1 = num<<1;
-    int result2 = num<<2;
-    int result3 = num<<3;
+    int result1 = num << 1;
+    int result2 = num << 2;
+    int result3 = num << 3;
 
     printf("1칸 이동결과 : %d \n", result1);
     printf("2칸 이동결과 : %d \n", result2);
     printf("3칸 이동결과 : %d \n", result3);
-    return 0;
 }
---
+++ sdf.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(void){
+    printf("DDDDDD");
+    return 0;
+}
