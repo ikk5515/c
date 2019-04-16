7dcd665b97dcadd94960a978dcc35ef4aea820b2
ikk5515 <ikk5515@naver.com>
2019-04-16 14:58:38 +0900

    Add files via upload

---
+++ C/CelFah100while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int c = 10;
+	int sum = 1;
+
+	printf("¼·¾¾     È­¾¾\n");
+	printf("-------------\n");
+	while (c <= 100 ) {
+		sum = 1.8*c + 32;
+		printf("%d         %d\n", c, sum);
+		c++;
+	}
+	return 0;
+}
