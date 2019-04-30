412e94e4013fde5a3c4aac8efab4b58e2ab6f165
ikk5515 <ikk5515@naver.com>
2019-04-30 15:14:40 +0900

    Add files via upload

---
+++ C/절대값1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int abs(int x) {
+	if (x >= 0) return (x);
+	else return (-x);
+}
+void main() {
+	int result;
+	result = abs(25);
+	printf("abs(25)=%d\n", result);
+	result = abs(-25);
+	printf("abs(-25)=%d\n", result);
+}
