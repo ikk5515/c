a9f25d4d509c15f79691751f2c092d79c842afe1
ikk5515 <ikk5515@naver.com>
2019-07-23 19:12:38 +0900

    C

---
+++ C/backarray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void){
+    int arr[6] = {1,2,3,4,5,6};
+    int *fptr = &arr[0];
+    int *bptr = &arr[5];
+    int i, temp;
+
+    for(i = 0; i<3; i++){
+        temp = *fptr;
+        *fptr = *bptr;
+        *bptr = temp;
+        fptr += 1;
+        bptr -= 1;
+    }
+    for(i=0; i<6; i++){
+        printf("%d " ,arr[i]);
+    }
+    return 0;
+}
