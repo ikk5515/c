0cb8cf33826038c9409226d91946488f2cd1439f
ikk5515 <ikk5515@naver.com>
2019-08-02 17:34:01 +0900

    C

---
+++ C/MemoryStringBack.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(void){
+    int maxlen, len, i;
+    char * str;
+    printf("문자열의 최대 길이 입력: ");
+    scanf("%d,", &maxlen);
+    getchar();                          // \n 문자 삭제
+    str = (char *)malloc(sizeof(char)*(maxlen+1));
+
+    printf("문자열 입력: ");
+    fgets(str, maxlen+1, stdin);
+    str[strlen(str)-1] = 0;            // \n 문자 삭제
+    len = strlen(str);
+
+    for(i = len; i>0; i--){
+        if(str[i] == ' '){
+            printf("%s", &str[i+1]);
+            printf(" ");
+            str[i] = 0;
+        }
+    }
+    printf("%s", &str[0]);
+    free(str);
+    return 0;
+}
