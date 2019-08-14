63ad3e805918c4b088479b36ac2676affc02b4c5
ikk5515 <ikk5515@naver.com>
2019-08-15 02:11:58 +0900

    C

--- C/1463.c
+++ C/1463.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
-int dp[100001];
+
+int dp[1000001];
 
 int min (int a, int b){
     return a > b ? b : a;
 }
 
 int main(void){
-    int n, i;
+    int n;
 
     scanf("%d", &n);
 
-    dp[0] = dp[1] = 0;
+    dp[1] = 0;
 
-    for(i = 2; i <= n; i++){
+    for(int i = 2; i <= n; i++){
         dp[i] = dp[i-1] + 1;
-        if(i%2==0) min(dp[i] = dp[i/2]+1);
-        if(i%3==0) min(dp[i] = dp[i/3]+1);
+        if(i%2 == 0) 
+            dp[i] = min(dp[i] , dp[i / 2] + 1);
+        if(i%3 == 0) 
+            dp[i] = min(dp[i] , dp[i / 3] + 1);
     }
-    printf("%d \n", n);
+    printf("%d \n", dp[n]);
+    return 0;
 }
