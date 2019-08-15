0095471a47510c3fe065a403ca3d0806232abafa
ikk5515 <ikk5515@naver.com>
2019-08-16 00:41:13 +0900

    C

---
+++ C/2869.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+    int A,B,V;
+
+    scanf("%d %d %d", &A, &B, &V);
+    int day;
+
+    day = (V - B - 1) / (A - B) + 1;
+
+    printf("%d",day);
+    return 0;
+}
