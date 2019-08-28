e1cdd7fe69b845a45724c91c191b02db4584924e
ikk5515 <ikk5515@naver.com>
2019-08-28 19:32:11 +0900

    C

--- C/9461.c
+++ C/9461.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int n;  // n<100
+int n; // n<100
 long long dp[101];
-int main(){
+int main()
+{
     int t;
     dp[1] = 1;
     dp[2] = 1;
@@ -10,13 +11,15 @@ int main(){
 
     scanf("%d", &n);
 
-    while(n--){
+    while (n--)
+    {
         scanf("%d", &t);
 
-        for(int i = 4; i <= t; i++){
-            dp[i] = dp[i-2] + dp[i-3];
+        for (int i = 4; i <= t; i++)
+        {
+            dp[i] = dp[i - 2] + dp[i - 3];
         }
-        printf("%lld \n",dp[t]);
+        printf("%lld \n", dp[t]);
     }
     return 0;
 }
