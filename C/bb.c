27834a7796b4c338795e481a72be4f8f0ea2cda6
Ingi Kim <ikk5515@naver.com>
2021-07-14 12:26:52 +0900

    C

--- C/bb.c
+++ C/bb.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main (){
-    int i, j, h;
-    scanf("%d %d", &i, &j);
-    for(h = i; i <= j; i++){
-        if (i % 55 == 0)
-            printf("FizzBuzz\n");
-        else if( i % 5 == 0)
-            printf("Fizz\n");
-        else if ( i % 11 == 0)
-            printf("Buzz\n");
-        else
-            printf("%d\n", i);
-    }
+int main()
+{
+    int *numPtr1;
+    int **numPtr2;
+    int num1;
+
+    scanf("%d", &num1);
+    numPtr1 = &num1;
+    numPtr2 = &numPtr1;
+
+
+    printf("%d\n", **numPtr2);
+
     return 0;
 }
