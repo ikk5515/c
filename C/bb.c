8ee8a54f04479d3d575aa35d350ed0a428952fed
Ingi Kim <ikk5515@naver.com>
2021-08-09 21:59:15 +0900

    cpp

--- C/bb.c
+++ C/bb.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Point2D {
-    int x;
-    int y;
+struct Point3D {
+    float x;
+    float y;
+    float z;
 };
 
-struct Point2D *allocPoint2D(){
-    struct Point2D *p = malloc(sizeof(struct Point2D));
+struct Point3D *allocPoint3D(float num1, float num2, float num3){
+    struct Point3D *p = malloc(sizeof(struct Point3D));
 
-    p->x = 90;
-    p->y = 75;
+    p->x = num1;
+    p->y = num2;
+    p->z = num3;
 
     return p;
+    free(p);
 }
 
 
 
- 
+
+
 int main()
 {
-    struct Point2D *pos1;
+    float x, y, z;
+    struct Point3D *pos1;
+   
+    scanf("%f %f %f", &x, &y, &z);
 
-    pos1 = allocPoint2D();
+    pos1 = allocPoint3D(x, y, z);
 
-    printf("%d %d\n", pos1->x, pos1->y);
+    printf("%f %f %f\n", pos1->x, pos1->y, pos1->z);
 
     free(pos1);
 
